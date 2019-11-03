c8e17f28cf423e15bbc769222592fb9d083eda09
Adam Wenger <adamwenger44@gmail.com>
2019-11-02 17:05:37 -0700

    made comments for others to understand

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// declaring variables
 var capitol = "QWERTYUIOPASDFGHJKLZXCVBNM";
 var lower = "qwertyuiopasdfghjklzxcvbnm";
 var spec = "!@#$%^&*()";
@@ -9,8 +10,9 @@ var special = confirm("do you want special characters?");
 var nums = confirm("do you want numbers?");
 var low = confirm("do you want lower case?");
 
-
+// main function
 function generate() {
+    // will only create password within criteria
     password = ""
     var final = ""
     if (howMany <= 7) {
@@ -25,25 +27,29 @@ function generate() {
         alert("your password will be " + howMany + " characters long")
 
     }
+    // if true will add capitol letters to options
 
     if (upper === true) {
         final += capitol;
     }
 
+    // if true will add special letters to options
 
     if (special === true) {
         final += spec
     }
 
+    // if true will add numbers to options
 
     if (nums === true) {
         final += num
     }
+    // if true will add lower letters to options
 
     if (low === true) {
         final += lower
     }
-
+// randomizing chices while making it as long as specified
     for (var i = 0; i < howMany; i++) {
         var rando = final[Math.floor(Math.random() * final.length)];
         password += rando
@@ -52,16 +58,19 @@ function generate() {
     document.getElementById("display").innerHTML = password
     
 }
+// running function
 generate()
+// grabbing button from html
 document.getElementById("myBtn").addEventListener("click", myFunction);
 document.getElementById("myBtn").addEventListener("click", secondFunction);
+// running functions to clear and make a new password based on same criteria
 function myFunction() {
   document.getElementById("display").textContent = "";
 }
 function secondFunction() {
     generate()
 }
-
+// function to copy password to clipboard
 function clip() {
     /* Get the text field */
     var copyText = document.getElementById("display");
